391be5a61605d2450e726f4b996e40fd83b909db
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-11 04:48:23 +0530

    done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 
 import express from "express"
 import cors from "cors"
-
+import cryptoRoute from "./route/cryptoRoutes"
+import deviationRoute from "./route/deviationRoute"
 
 const app=express()
 
 
 app.use(express(),cors())
+
+
+app.use("/koinx/api/v1",cryptoRoute)
+app.use("/koinx/api/v1",deviationRoute)
 
 export default app
