82427ff6b47e1c9e6c06cf5539a36090dd9e6450
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-11 16:50:19 +0530

    fixed

--- src/utils/cronJob.ts
+++ src/utils/cronJob.ts
@@ -1,4 +1,5 @@
 import cron from 'node-cron';
 import fetchCryptoData from './fetchCryptoData';
 
-cron.schedule('0 */2 * * *', fetchCryptoData);
+cron.schedule('0 */2 * * *', fetchCryptoData); // exceute every 2 hr
+// cron.schedule('*/10 * * * * *', fetchCryptoData); for testing for 10sec
